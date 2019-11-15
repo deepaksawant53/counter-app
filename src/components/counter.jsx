71600584e255349cddce312e39d353d96d905dc9
Deepak Sawant <deepak.sawant53@gmail.com>
2019-11-16 00:32:49 +0530

    Added logic for conditional rendering list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,29 +3,24 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 1,
-    tags: ['tag1', 'tag2', 'tag3']
+    tags: []
   };
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+  }
   render() {
     return (
+      // When a javascript engine tries to evaluate this operation it will evaluate the 
+      //   first opearation which will return true now it will try to evaluate the second operation but it is not a boolean value. So the javascript engine 
+      //   will try to convert it in truthy or falsy value. In javascript empty string is falsy here our string has characters so it returns true and hence 
+      //   it returns the last value. So if there are 3 operations and all the 3 operations return true then it will print the 3 operand.
       <div>
-        <span style={{ fontSize: 10 }} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </div>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 export default Counter;
