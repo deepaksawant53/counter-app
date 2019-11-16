b6e1e814fa2e2672aaa85c6d749380746bc21065
Deepak Sawant <deepak.sawant53@gmail.com>
2019-11-16 13:36:38 +0530

    Made changes for binding events to a element.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,25 +2,47 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 1,
-    tags: []
+    count: 1
   };
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+  constructor() {
+    super();
+    console.log("Constructor", this);
+    this.handleIncrement = this.handleIncrement.bind(this);
   }
+
+  handleIncrement() {
+    console.log("Increment clicked", this);
+  }
+
+
   render() {
     return (
-      // When a javascript engine tries to evaluate this operation it will evaluate the 
-      //   first opearation which will return true now it will try to evaluate the second operation but it is not a boolean value. So the javascript engine 
-      //   will try to convert it in truthy or falsy value. In javascript empty string is falsy here our string has characters so it returns true and hence 
-      //   it returns the last value. So if there are 3 operations and all the 3 operations return true then it will print the 3 operand.
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span style={{ fontSize: 10 }} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        { /* 
+          Below mentioned methods can also be used to bind events
+          <button onClick={ () => console.log("Increment clicked", this) } className="btn btn-secondary btn-sm">Increment</button>
+          OR
+          <button onClick={ this.handleIncrement.bind(this) } className="btn btn-secondary btn-sm">Increment</button>
+          OR
+          We can change handleIncrement method to a lambda like handleIncrement = () => console.log("Increment clicked", this) and then in our button element
+          <button onClick={ this.handleIncrement } className="btn btn-secondary btn-sm">Increment</button>*/
+        }
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 export default Counter;
