26ded09142c6b09e1d7c8008699444471cd5b23a
Deepak Sawant <deepak.sawant53@gmail.com>
2019-11-16 18:17:26 +0530

    Passing arguments to the handler method.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,7 +10,8 @@ class Counter extends Component {
     this.handleIncrement = this.handleIncrement.bind(this);
   }
 
-  handleIncrement() {
+  handleIncrement(product) {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   }
 
@@ -19,7 +20,7 @@ class Counter extends Component {
     return (
       <div>
         <span style={{ fontSize: 10 }} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={() => this.handleIncrement({ id: 1 })} className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
