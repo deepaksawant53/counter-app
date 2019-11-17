29fdccbd4558466f1528c75d71a28816c45c6a45
Deepak Sawant <deepak.sawant53@gmail.com>
2019-11-17 19:30:33 +0530

    Changes done to demonstrate passing values to component.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,14 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value
   };
 
-  constructor() {
-    super();
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
-
-  handleIncrement(product) {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  }
-
+  handleIncrement = (product) => this.setState({ value: this.state.value + 1 });
 
   render() {
+    console.log("props", this.props);
+
     return (
       <div>
         <span style={{ fontSize: 10 }} className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -27,12 +20,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += (this.state.value === 0) ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    console.log(this.state.test);
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
