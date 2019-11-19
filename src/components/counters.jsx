77b3f4f4bfb5bbcf4fac1e9304aed8a241e2317a
Deepak Sawant <deepak.sawant53@gmail.com>
2019-11-19 08:34:11 +0530

    Changes done to add support for Single source of truth by removing local state from counter and making counter a controlled component which is controlled by Counters.jsx.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -12,16 +12,36 @@ class Counters extends Component {
     ]
   }
 
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    // We directly dont do "counters[0].value++;" because array has reference to counter objects
+    // and updating a reference would directly make updates in object and in react directly updating and value is a no-no
+    // hence we get the counter passed as a argument and then place it inside counter[0] and make the changes.
+    counters[index] = { ...counter }
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
   handleDelete = (counterId) => {
     console.log('Event Handler Called', counterId);
     this.setState({ counters: this.state.counters.filter(counter => counter.id !== counterId) });
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
         {this.state.counters.map(counter =>
-          <Counter key={counter.id} counter={counter} onDelete={this.handleDelete} />
+          <Counter key={counter.id} counter={counter} onIncrement={this.handleIncrement} onDelete={this.handleDelete} />
         )};
       </div>
     );
