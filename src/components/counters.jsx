c2514263c0790842419cdf4fc6720beb72a4461e
Deepak Sawant <deepak.sawant53@gmail.com>
2019-11-19 23:16:48 +0530

    Object restructuring done in navbar and counters.jsx

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,11 +4,12 @@ import { log } from 'util';
 
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
-        <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
-        {this.props.counters.map(counter =>
-          (<Counter key={counter.id} counter={counter} onIncrement={this.props.onIncrement} onDelete={this.props.onDelete} />
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+        {counters.map(counter =>
+          (<Counter key={counter.id} counter={counter} onIncrement={onIncrement} onDelete={onDelete} />
           ))}
       </div>
     );
